67d422bf4e55c6f5c71db78ab23d68158e2279f5
im4nuj <pandey.anuj09@gmail.com>
2023-01-16 14:42:54 +0530

    removed script file

--- inside_script.sh
+++
@@ -1,7 +0,0 @@
-sudo yum install git
-sudo yum install java-1.8.0-openjdk-devel
-sudo yum install apache-maven
-mkdir daemon
-cd daemon
-git pull https://github.com/im4nuj/AWS-demo.git
-mvn clean install
